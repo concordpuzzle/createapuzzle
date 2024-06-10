69fea5b3d62196f6e4482b88070dd18e00e89627
jeremy-marple <>
2024-06-10 10:16:45 -0400

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -77,7 +77,7 @@
                                     <i class="fa-solid fa-palette" style="color:#fff;font-size:22px;"></i>
                                 </div>
 
-                                <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">Generate Puzzle Images</h2>
+                                <h2 class="mt-6 text-xl font-semibold text-gray-900">Generate Puzzle Images</h2>
 
                                 <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
                                     Laravel has wonderful documentation covering every aspect of the framework. Whether you are a newcomer or have prior experience with Laravel, we recommend reading our documentation from beginning to end.
@@ -95,7 +95,7 @@
                                     <i class="fa-solid fa-star" style="color:#fff;font-size:22px;"></i>
                                 </div>
 
-                                <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">Rate Puzzle Images</h2>
+                                <h2 class="mt-6 text-xl font-semibold text-gray-900">Rate Puzzle Images</h2>
 
                                 <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
                                     Laracasts offers thousands of video tutorials on Laravel, PHP, and JavaScript development. Check them out, see for yourself, and massively level up your development skills in the process.
