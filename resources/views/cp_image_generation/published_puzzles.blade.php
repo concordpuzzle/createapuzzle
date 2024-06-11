e6421d7adbfedb3c37a171b8a891218094e00bad
jeremy-marple <>
2024-06-11 16:12:06 -0400

    Update published_puzzles.blade.php

--- resources/views/cp_image_generation/published_puzzles.blade.php
+++ resources/views/cp_image_generation/published_puzzles.blade.php
@@ -5,9 +5,9 @@
     <h1 class="text-4xl my-8">Published Puzzles</h1>
     <div class="flex flex-wrap justify-center">
         @foreach($publishedProducts as $product)
-            <div class="w-1/3 md:w-1/3 lg:w-1/4 p-4">
+            <div class="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-4">
                 <div class="bg-white rounded-lg shadow-md overflow-hidden">
-                    <img src="{{ Storage::url($product->cropped_image) }}" class="w-full h-64 object-cover" alt="{{ $product->title }}">
+                    <img src="{{ Storage::url($product->cropped_image) }}" class="w-full h-64 object-cover" alt="{{ $product->title }}" style="border-radius: 4px;">
                     <div class="p-4">
                         <h5 class="text-lg font-bold mb-2">{{ $product->title }}</h5>
                         <p class="text-gray-600 mb-4">{{ $product->description }}</p>
