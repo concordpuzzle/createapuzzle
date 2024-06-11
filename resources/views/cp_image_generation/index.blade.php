7c4be957cfc8c390d480bd41b1972471ae29a23a
jeremy-marple <>
2024-06-10 23:53:32 -0400

    Update index.blade.php

--- resources/views/cp_image_generation/index.blade.php
+++ resources/views/cp_image_generation/index.blade.php
@@ -1,10 +1,7 @@
-
 <!-- resources/views/cp_image_generation/index.blade.php -->
 @extends('layouts.app')
 
 @section('content')
-
-
 <link href="https://cdnjs.cloudflare.com/ajax/libs/cropperjs/1.5.12/cropper.min.css" rel="stylesheet">
 <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
 
@@ -45,7 +42,7 @@
     </div>
 
     <!-- Crop Modal -->
-    <div id="cropModal" class="modal" tabindex="-1" role="dialog">
+    <div id="cropModal" class="modal fade" tabindex="-1" role="dialog">
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header">
@@ -58,24 +55,24 @@
                     <img id="imageToCrop" src="" style="max-width: 100%;">
                 </div>
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-primary" onclick="cropImage()">Crop</button>
+                    <button type="button" class="btn btn-primary" id="cropButton">Crop</button>
                     <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
                 </div>
             </div>
         </div>
     </div>
-
-    <!-- Canvas to hold cropped image data -->
-    <canvas id="croppedCanvas" style="display:none;"></canvas>
 </div>
+
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/cropperjs/1.5.12/cropper.min.js"></script>
 <script>
     let cropper;
+    
     function openCropModal(imageUrl) {
-        document.getElementById('imageToCrop').src = imageUrl;
+        $('#imageToCrop').attr('src', imageUrl);
         $('#cropModal').modal('show');
+        
         $('#cropModal').on('shown.bs.modal', function () {
             cropper = new Cropper(document.getElementById('imageToCrop'), {
                 aspectRatio: 16 / 9,  // Change to your desired ratio
@@ -87,16 +84,13 @@
         });
     }
 
+    $('#cropButton').on('click', function() {
+        cropImage();
+    });
+
     function cropImage() {
         const canvas = cropper.getCroppedCanvas();
-        const croppedImage = canvas.toDataURL('image/png');
-        const croppedCanvas = document.getElementById('croppedCanvas');
-        const context = croppedCanvas.getContext('2d');
-        croppedCanvas.width = canvas.width;
-        croppedCanvas.height = canvas.height;
-        context.drawImage(canvas, 0, 0);
-
-        // Send the cropped image data to the server
+        
         canvas.toBlob(function(blob) {
             const formData = new FormData();
             formData.append('cropped_image', blob, 'cropped.png');
@@ -119,6 +113,4 @@
         });
     }
 </script>
-<!-- resources/views/cp_image_generation/index.blade.php -->
-
 @endsection
