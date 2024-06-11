88e8b2f003c44b889853752511b64cf286adab8e
jeremy-marple <>
2024-06-11 14:24:53 -0400

    Update cropped.blade.php

--- resources/views/cp_image_generation/cropped.blade.php
+++ resources/views/cp_image_generation/cropped.blade.php
@@ -9,12 +9,10 @@
             <form method="POST" action="{{ route('cp_image_generation.create_product') }}">
                 @csrf
                 <div class="form-group">
-                    <label for="title">Title</label>
-                    <input type="text" class="form-control" id="title" name="title" value="{{ old('title') }}" required>
+                    <input type="text" class="form-control" id="title" name="title" placeholder="Title" value="{{ old('title') }}" required>
                 </div>
                 <div class="form-group">
-                    <label for="description">Description</label>
-                    <textarea class="form-control" id="description" name="description" rows="3" required>{{ old('description') }}</textarea>
+                    <textarea class="form-control" id="description" name="description" rows="3" placeholder="Description" required>{{ old('description') }}</textarea>
                 </div>
                 <input type="hidden" name="image_id" value="{{ $image->id }}">
                 <button type="submit" class="btn btn-primary">Create Puzzle</button>
