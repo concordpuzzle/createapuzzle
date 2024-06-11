bcc9241f60cd2c3cfa9852e3a1b202e4458b6761
jeremy-marple <>
2024-06-11 16:19:36 -0400

    Update published_puzzles.blade.php

--- resources/views/cp_image_generation/published_puzzles.blade.php
+++ resources/views/cp_image_generation/published_puzzles.blade.php
@@ -3,15 +3,15 @@
 @section('content')
 <div class="container mx-auto text-center">
     <h1 class="text-4xl my-8">Published Puzzles</h1>
-    <div class="flex flex-wrap justify-center mx-4">
+    <div class="flex flex-wrap justify-center">
         @foreach($publishedProducts as $product)
-            <div class="w-1/4 p-4">
+            <div class="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-4">
                 <div class="bg-white rounded-lg shadow-md overflow-hidden mb-4">
-                    <img src="{{ Storage::url($product->cropped_image) }}" class="w-full h-64 object-cover" alt="{{ $product->title }}" style="border-radius: 4px;">
+                    <img src="{{ Storage::url($product->cropped_image) }}" class="w-full h-64 object-cover rounded-t-lg" alt="{{ $product->title }}">
                     <div class="p-4">
                         <h5 class="text-lg font-bold mb-2">{{ $product->title }}</h5>
                         <p class="text-gray-600 mb-4">{{ $product->description }}</p>
-                        <a href="{{ $product->product_url }}" class="inline-block bg-red-700 text-white py-2 px-4 rounded" target="_blank">View Product</a>
+                        <a href="{{ $product->product_url }}" class="inline-block bg-red-700 text-white py-2 px-4 rounded mt-2" target="_blank">View Product</a>
                     </div>
                 </div>
             </div>
