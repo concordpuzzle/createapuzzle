c27cc1d3e4a7a1ff46e8abacee36b7b4fbec6148
jeremy-marple <>
2024-06-12 17:12:44 -0400

    Update puzzle_feed.blade.php

--- resources/views/cp_image_generation/puzzle_feed.blade.php
+++ resources/views/cp_image_generation/puzzle_feed.blade.php
@@ -35,6 +35,10 @@
         font-style: italic;
         font-size: 9px;
     }
+    .card-link {
+        text-decoration: none;
+        color: inherit;
+    }
 </style>
 
 <div class="container text-center my-5">
@@ -42,23 +46,25 @@
     <div class="row">
         @foreach($publishedProducts as $product)
             <div class="col-md-3 mb-4">
-                <div class="card h-100 shadow-sm">
-                    <img src="{{ Storage::url($product->cropped_image) }}" class="card-img-top" alt="{{ $product->title }}" style="border-radius: 4px;">
-                    <div class="card-body d-flex flex-column">
-                        <h5 class="card-title radio-canada-big">
-                            {{ $product->title }}
-                            <button class="like-button {{ $product->likes->contains('user_id', auth()->id()) ? 'liked' : '' }}" onclick="likeProduct({{ $product->id }}, this)">
-                                <i class="fa fa-heart{{ $product->likes->contains('user_id', auth()->id()) ? '' : '-o' }}"></i>
-                            </button>
-                        </h5>
-                        <div class="like-count">
-                            <span id="like-count-{{ $product->id }}">{{ $product->likes->count() }}</span> people like this
-                        </div>
-                        <div class="mt-auto">
-                            <a href="{{ $product->product_url }}" class="btn btn-danger btn-block mt-2" target="_blank">View Product</a>
+                <a href="{{ $product->product_url }}" class="card-link" target="_blank">
+                    <div class="card h-100 shadow-sm">
+                        <img src="{{ Storage::url($product->cropped_image) }}" class="card-img-top" alt="{{ $product->title }}" style="border-radius: 4px;">
+                        <div class="card-body d-flex flex-column">
+                            <h5 class="card-title radio-canada-big">
+                                {{ $product->title }}
+                                <button class="like-button {{ $product->likes->contains('user_id', auth()->id()) ? 'liked' : '' }}" onclick="likeProduct({{ $product->id }}, this)">
+                                    <i class="fa fa-heart{{ $product->likes->contains('user_id', auth()->id()) ? '' : '-o' }}"></i>
+                                </button>
+                            </h5>
+                            <div class="like-count">
+                                <span id="like-count-{{ $product->id }}">{{ $product->likes->count() }}</span> people like this
+                            </div>
+                            <div class="like-count">
+                                Click to view this puzzle
+                            </div>
                         </div>
                     </div>
-                </div>
+                </a>
             </div>
         @endforeach
     </div>
