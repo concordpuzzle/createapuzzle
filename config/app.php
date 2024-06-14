5fb50989db69959b4aa3c23b18463663f9a3960b
jeremy-marple <>
2024-06-13 20:25:04 -0400

    Update app.php

--- config/app.php
+++ config/app.php
@@ -186,7 +186,7 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'Example' => App\Facades\Example::class,
-        'Image' => Intervention\Image\Facades\Image::class,
+        'Imagegeneration' => Intervention\Image\Facades\Imagegeneration::class,
     ])->toArray(),
 
 
