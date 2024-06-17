a6887fa9f01a918dc3860dced2655aa1ffc08757
jeremy-marple <>
2024-06-17 19:08:17 -0400

    Update public.blade.php

--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -52,21 +52,19 @@
 
         <!-- Styles -->
         @livewireStyles
+        @livewire('navigation-menu')
+
+<!-- Page Heading -->
+@if (isset($header))
+    <header class="bg-white shadow">
+        <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+            {{ $header }}
+        </div>
+    </header>
+@endif
     </head>
     <body>
-    @if (Route::has('login'))
-                <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
-                    @auth
-                        <a href="{{ url('/dashboard') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</a>
-                    @else
-                        <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</a>
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
             <div class="flex justify-center"><br>
                     <a href="https://make.concordpuzzle.com"> <div style="color:#b71540;font-size:33px;" class="arvo-bold">Make a Puzzle</div></a>
                 </div>
