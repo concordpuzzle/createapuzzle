8fc685f04b7f82f77d9e54cccfd0804b50492113
jeremy-marple <>
2024-06-17 20:29:10 -0400

    Update public.blade.php

--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -52,7 +52,16 @@
 
         <!-- Styles -->
         @livewireStyles
+        @livewire('navigation-menu')
 
+<!-- Page Heading -->
+@if (isset($header))
+    <header class="bg-white shadow">
+        <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+            {{ $header }}
+        </div>
+    </header>
+@endif
     </head>
     <body>
 
