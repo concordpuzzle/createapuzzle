96f07fef55235f0ebd16636a053867edfc5ed487
jeremy-marple <>
2024-06-23 01:26:20 -0400

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -117,7 +117,7 @@
     </svg>
 </a>
 
-<a href="#" class="scale-100 p-6 bg-white rounded-lg shadow-none shadow-gray-500/20 flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
+<a href="https://concordpuzzle.com/shop/500-piece-puzzle-of-a-picture/" class="scale-100 p-6 bg-white rounded-lg shadow-none shadow-gray-500/20 flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
     <div>
         <div style="background-color:#0c2461" class="h-16 w-16 bg-red-50 dark:bg-red-800/20 flex items-center justify-center rounded-full">
             <i class="fa-solid fa-camera-retro" style="color:#fff;font-size:22px;"></i>
