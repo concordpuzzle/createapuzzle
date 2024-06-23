2b12851b6430d55ff94f4173e24bb81870d57c22
jeremy-marple <>
2024-06-23 01:22:58 -0400

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -90,7 +90,7 @@
                     <a href="https://make.concordpuzzle.com/dashboard" class="scale-100 p-6 bg-white rounded-lg shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
     <div>
         <div style="background-color:#0c2461" class="h-16 w-16 flex items-center justify-center rounded-full">
-            <i style="color:#fff;font-size:22px;">⭐</i>
+            <p style="color:#fff;font-size:22px;">⭐</p>
         </div>
         <h2 class="mt-6 text-xl font-semibold text-gray-900">Make a Puzzle with <u>AI</u></h2>
         <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
